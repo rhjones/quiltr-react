146af032f6f6b6d2163a5fcb2cac8502a5b916b7
rhjones <rebekah.heacock@gmail.com>
2016-11-29 17:12:53 -0500

    Implement basic routing
    Add react-router
    Configure base route (/) and IndexRoute (maps to Home component)
    Create simple Home component to test route

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>Quiltr</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+
 import App from './App';
+import Home from './Home';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+    </Route>
+  </Router>
+), document.body)
