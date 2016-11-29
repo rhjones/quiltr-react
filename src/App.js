0b57843cb34bd8237565a76f56176dd0eeb1da54
rhjones <rebekah.heacock@gmail.com>
2016-11-29 18:02:52 -0500

    Add routes for patterns and pattern
    Add routes to index.js for patterns and patterns/:id
    Add links to Home and Patterns views from App.js
    Render into div#root in index.html instead of document.body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/patterns">Patterns</Link></li>
+        </ul>
         {this.props.children}
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 
 import App from './App';
-import Home from './Home';
+import Home from './modules/Home';
+import Patterns from './modules/Patterns';
+import Pattern from './modules/Pattern';
 import './index.css';
 
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
+      <Route path="patterns" component={Patterns} />
+      <Route path="patterns/:id" component={Pattern} />
     </Route>
   </Router>
-), document.body)
+), document.getElementById('root'));
