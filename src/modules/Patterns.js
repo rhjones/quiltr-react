e0c3c6c38c3e5b7f90e9837a3f159c88bf38b9ef
rhjones <rebekah.heacock@gmail.com>
2016-11-30 11:55:23 -0500

    Get patterns data
    Make fetch request when mounting Patterns component
    Request gets all patterns and sets this.state.patterns equal to response.json()
    
    Using environment variable to set API host; may revisit this later to ensure that this works across local dev environments and in production

--- src/modules/Patterns.js
+++ src/modules/Patterns.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
 
 class Patterns extends Component {
+	constructor() {
+    super();
+    this.state = { patterns: [] };
+  }
+
+  status(response) {  
+	  if (response.status >= 200 && response.status < 300) {  
+	    return Promise.resolve(response);
+	  } else {  
+	    return Promise.reject(new Error(response.statusText));
+	  }  
+	}
+
+	json(response) {  
+	  return response.json();
+	}
+
+  componentDidMount() {
+    fetch(process.env.REACT_APP_API_HOST + 'patterns')
+      .then(this.status)
+      .then(this.json)
+      .then((json) => this.setState({patterns: json}))
+      .then(() => console.log(this.state.patterns))
+      .catch(console.error)
+      ;
+  }
+
   render() {
     return (
       <div>
